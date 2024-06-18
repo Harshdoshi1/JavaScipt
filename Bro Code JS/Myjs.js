fa8d245da94226f2e04e9c00e1bc2e5b84a413f6
Harsh Doshi <115213620+Harshdoshi1@users.noreply.github.com>
2024-06-18 14:52:14 +0530

    push

--- Bro Code JS/Myjs.js
+++ Bro Code JS/Myjs.js
@@ -431,6 +431,8 @@
 // random cards 
 
 let cards = ["A","1","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+shuffle(array);
  
 function shuffle(array){
     let currentindex = array.length();
