cdae0e4b9a92dd15653ce4cf34bbc879549cb488
Harsh Doshi <115213620+Harshdoshi1@users.noreply.github.com>
2024-06-18 16:22:56 +0530

    push

--- Bro Code JS/Myjs.js
+++ Bro Code JS/Myjs.js
@@ -485,4 +485,24 @@
 
 // store.forEach((value, key) => console.log(`${key}` ,$,{value})); // error
 
-//objes
+//object
+
+const car = {
+    model : "mustang",
+    color : "red",
+    year : 2024,
+
+    drive : function(){
+        console.log("you drive the car");
+    },
+    break : function(){
+        console.log("you break the car");
+    }
+}
+
+console.log(car.model);
+console.log(car.color);
+console.log(car.year);
+
+car.drive();
+car.break();
